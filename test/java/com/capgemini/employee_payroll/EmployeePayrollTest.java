181c90b946db917e60d5b1c475c59eba11925ea7
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-06 08:17:45 -0800

    added :New employee is added into Json Server

--- test/java/com/capgemini/employee_payroll/EmployeePayrollTest.java
+++ test/java/com/capgemini/employee_payroll/EmployeePayrollTest.java
@@ -30,11 +30,35 @@ public class EmployeePayrollTest {
 	}
 
 	public Employee_payroll_Data[] getEmployeeList() {
-		Response response = RestAssured.get("/payroll_service");
+		Response response = RestAssured.get("/payroll");
 		System.out.println("employee payroll entries in jsonserver:\n" + response.asString());
 		Employee_payroll_Data[] arrayOfEmployees = new Gson().fromJson(response.asString(),
 				Employee_payroll_Data[].class);
 		return arrayOfEmployees;
 	}
 
+	@Test
+	public void givenNewEmpolyee_WhenAdded_ShouldMatch201ResponseAndCount() throws EmployeePayrollException {
+		Response response = RestAssured.get("/payroll");
+		System.out.println("Employee Payroll entries in JSONserver" + response.asString());
+		Employee_payroll_Data[] arrayOfEmps = new Gson().fromJson(response.asString(), Employee_payroll_Data[].class);
+		Employee_payroll_Data employeePayrollData = new Employee_payroll_Data(0, "Anila", 500000.00, LocalDate.now(),
+				"F");
+		Response response1 = addEmployeeToJsonServer(employeePayrollData);
+		int statusCode = response1.getStatusCode();
+		Assert.assertEquals(201, statusCode);
+		Employee_payroll_service employeePayrollSevice;
+		employeePayrollSevice = new Employee_payroll_service(Arrays.asList(arrayOfEmps));
+		employeePayrollSevice.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
+		long entries = employeePayrollSevice.countEntries(IOService.REST_IO);
+		Assert.assertEquals(8, entries);
+	}
+
+	private Response addEmployeeToJsonServer(Employee_payroll_Data empPayrollData) {
+		String empJson = new Gson().toJson(empPayrollData);
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.body(empJson);
+		return request.post("payroll");
+	}
 }
